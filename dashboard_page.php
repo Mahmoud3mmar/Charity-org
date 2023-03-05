a2d2b9168b97c7b275cd295d7152436f242d0d60
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-03-05 16:57:53 +0200

    tables style added

--- dashboard_page.php
+++ dashboard_page.php
@@ -20,6 +20,7 @@ session_start();
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/Tables.css">
 
 </head>
 <body>
@@ -33,9 +34,121 @@ session_start();
       
       <a href="adding_worker_page.php" class="btn">Add New Worker</a>
       <!-- <a href="logout.php" class="btn">logout</a> -->
+
+
+      
    </div>
 
-</div>
 
+
+
+   
+</div>
+<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6 text-center mb-5">
+					<h2 class="heading-section"></h2>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12">
+					<div class="table-wrap">
+						<table class="table table-striped">
+						  <thead>
+						    <tr>
+						      <th>Invoce</th>
+						      <th>Customer</th>
+						      <th>Ship</th>
+						      <th>Price</th>
+						      <th>Pruchased Price</th>
+						      <th>Status</th>
+						    </tr>
+						  </thead>
+						  <tbody>
+						    <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-success">Progress</a></td>
+						    </tr>
+
+						    <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-warning">Open</a></td>
+						    </tr>
+
+						    <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-danger">On hold</a></td>
+						    </tr>
+
+						    <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-success">Progress</a></td>
+						    </tr>
+
+						     <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-danger">On hold</a></td>
+						    </tr>
+
+						     <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-warning">Open</a></td>
+
+						       <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-warning">Open</a></td>
+						    </tr>
+
+						    <tr>
+						      <th scope="row">1001</th>
+						      <td>Mark Otto</td>
+						      <td>Japan</td>
+						      <td>$3000</td>
+						      <td>$1200</td>
+						      <td><a href="#" class="btn btn-success">Progress</a></td>
+						    </tr>
+						  </tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+   <script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
 </body>
 </html>
