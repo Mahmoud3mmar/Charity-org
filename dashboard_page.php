38b077f774bfe697d5fff26a671e9afdb3a9cc6e
Mahmoud3mmar <55957447+Mahmoud3mmar@users.noreply.github.com>
2023-03-07 12:32:39 +0200

    dashboard workers page

--- dashboard_page.php
+++ dashboard_page.php
@@ -32,19 +32,17 @@ session_start();
       <h1>welcome <span><?php echo $_SESSION['admin_name'] ?></span></h1>
       <p>this is an admin page</p>
       
-      <a href="adding_worker_page.php" class="btn">Add New Worker</a>
+      <a href="workers_page.php" class="btn"> Worker</a>
       <!-- <a href="logout.php" class="btn">logout</a> -->
 
 
+      <button id="show-table" class="btn">Show Table</button>
+      <!-- <table id="my-table" style="display:none;"> -->
       
    </div>
 
-
-
-
-   
-</div>
-<section class="ftco-section">
+   <!-- table html -->
+   <section class="ftco-section">
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
@@ -54,7 +52,7 @@ session_start();
 			<div class="row">
 				<div class="col-md-12">
 					<div class="table-wrap">
-						<table class="table table-striped">
+						<table class="table table-striped" id="my-table" style="display:none;" >
 						  <thead>
 						    <tr>
 						      <th>Invoce</th>
@@ -143,6 +141,24 @@ session_start();
 			</div>
 		</div>
 	</section>
+   
+   
+   <!-- aims to show the table when button clicked  -->
+   <script>
+   var button = document.getElementById("show-table");
+   var table = document.getElementById("my-table");
+   
+   button.addEventListener("click", function() {
+      if (table.style.display === "none") {
+         table.style.display = "table";
+      } else {
+         table.style.display = "none";
+      }
+   });
+   </script>
+   
+</div>
+
 
 
 
